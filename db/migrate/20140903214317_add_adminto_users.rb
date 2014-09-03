e8ed9e683f36329f86d6cd03a3e4341ea045a2bb
Jesus Brazon <j.brazon@me.com>
2014-09-03 17:44:01 -0400

    added admin boolean to users

---
+++ db/migrate/20140903214317_add_adminto_users.rb
@@ -0,0 +1,4 @@
+class AddAdmintoUsers < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903205501) do
+ActiveRecord::Schema.define(version: 20140903214317) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
